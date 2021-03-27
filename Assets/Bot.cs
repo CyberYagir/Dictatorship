c6271ca85ce0c27e7a52a0d556dad75e22cf82c0
TupiDebil <47659509+TupiDebil@users.noreply.github.com>
2021-03-27 19:55:12 +0300

    Добавил картинку

--- Assets/Bot.cs
+++ Assets/Bot.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-
+//11
 public class Bot : MonoBehaviour
 {
     public AIDestinationSetter path;
